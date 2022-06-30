31185ad14ec3d03b93077833550ef1701a04b899
Rogers Setiady <setiady.rogers28@gmail.com>
2022-06-30 20:39:48 +0700

    bugfixing

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useState, useEffect } from "react";
 import Image from 'next/image';
-import dogceo from '../public/dog-ceo-zine.jpeg'
 
 export default function Home({ breeds = [] }) {
   const [breedList, setBreedList] = useState([]);
@@ -25,10 +24,6 @@ export default function Home({ breeds = [] }) {
         <h2>Dog CEO Zine.</h2>
         <p>A business and lifestyle magazine for the modern dog.</p>
         <p>Issue 1 out now. <a href="https://sideorders.co.uk/collections/frontpage/products/dog-ceo-issue-1" target="_blank">Buy now</a> from Side Orders Publishing. Ships worldwide.</p>
-        <Image 
-        src={dogceo} 
-        alt="Dog CEO"
-        />
         <p>Choose Your Dog Breed : </p>
         <select>
           {breedList.map((breed) => (
